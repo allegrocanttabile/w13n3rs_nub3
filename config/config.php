3c3be98f854c1220865535c142a18e965ef1b839
allegro <allegrocanttabile@gmail.com>
2019-08-15 18:01:49 -0500

    subiendo al git

---
+++ config/config.php
@@ -0,0 +1,20 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ock9yy9ns2fe',
+  'passwordsalt' => 'O2oydvSdpnVDkoCk/JO61uc18udyim',
+  'secret' => 'MF.i5DDwXy88ihgRX6qmDk0eVc7b7eyI.SvYEk2P3wf38CuE',
+  'trusted_domains' => 
+  array (
+    0 => 'nube.wienerstech.com',
+  ),
+  'datadirectory' => '/home2/wienerstech/nube.wienerstech.com/data',
+  'overwrite.cli.url' => 'http://nube.wienerstech.com',
+  'dbtype' => 'mysql',
+  'version' => '8.0.16.3',
+  'dbname' => 'wienerst_nube',
+  'dbhost' => 'localhost',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'wienerst_nube',
+  'dbpassword' => 'W86c9x6xg6$',
+  'installed' => true,
+);
